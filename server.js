be851ed9bee651046842f212a1499b01c0a997cd
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-03 02:50:38 +0300

    finished the back-end

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 "use strict";
 
-const { request, response } = require("express");
 const express = require("express");
-require("cors");
 require("dotenv").config();
+const weatherData = require("./data/weather.json");
 const server = express();
+const cors = require("cors");
 const PORT = process.env.PORT;
+let arrayOfObjects = [];
+
+server.use(cors());
 
 // http://localhost:3010/
 server.get("/", (request, response) => {
   response.send("Hello from the server");
+});
+
+// http://localhost:3010/weather?q=<cityname>&lon=<the_longitude>&lat=<the_latitude>
+server.get("/weather", (request, response) => {
+  let name = request.query.q;
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  let lon = request.query.lon;
+  let lat = request.query.lat;
+
+  let weatherArr = weatherData.find((element) => name == element.city_name);
+  let dataArray = weatherArr.data.map((element) => {
+    let obj = {};
+    obj.date = element.datetime;
+    obj.description = element.weather.description;
+    return obj;
+  });
+
+  // class Forecast {
+  //   constructor(date, description) {
+  //     this.date = date;
+  //     this.description = description;
+  //     arrayOfObjects.push(this);
+
+  //   }
+  // }
+
+  response.send(dataArray);
+});
+
+server.get("*", (req, res) => {
+  res.status(404).send("page not found");
 });
 
 server.listen(PORT, () => {
