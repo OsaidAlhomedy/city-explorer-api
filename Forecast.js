7276a09cdb7f3eecaaeb5b6a27584ab779868ad3
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-03 23:58:49 +0300

    finished the weather api settings

--- Forecast.js
+++
@@ -1,8 +0,0 @@
-class Forecast {
-  constructor(date, description) {
-    this.date = date;
-    this.description = description;
-  }
-}
-
-module.exports = Forecast;
--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 
 const express = require("express");
 require("dotenv").config();
-const weatherData = require("./data/weather.json");
 const server = express();
+const axios = require("axios");
 const cors = require("cors");
 const PORT = process.env.PORT;
-const Forecast = require("./Forecast");
 
 server.use(cors());
+
+class Forecast {
+  constructor(date, desc, low, high) {
+    this.date = date;
+    this.description = desc;
+    this.low = low;
+    this.high = high;
+  }
+}
 
 // http://localhost:3010/
 server.get("/", (request, response) => {
@@ -16,33 +24,35 @@ server.get("/", (request, response) => {
 });
 
 // http://localhost:3010/weather?q=<cityname>&lon=<the_longitude>&lat=<the_latitude>
-server.get("/weather", (request, response) => {
-  let arrayOfObjects = [];
+
+server.get("/weather", weatherHandle);
+
+async function weatherHandle(request, response) {
   let name = request.query.q;
   name = name.charAt(0).toUpperCase() + name.slice(1);
-  let lon = request.query.lon;
-  let lat = request.query.lat;
+  const lon = request.query.lon;
+  const lat = request.query.lat;
+  const URL = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
 
-  let weatherArr = weatherData.find((element) => name == element.city_name);
+  axios
+    .get(URL)
+    .then((result) => {
+      let weatherData = result.data.data;
 
-  for (let i = 0; i < weatherArr.data.length; i++) {
-    arrayOfObjects.push(
-      new Forecast(
-        weatherArr.data[i].datetime,
-        weatherArr.data[i].weather.description
-      )
-    );
-  }
-
-  // let dataArray = weatherArr.data.map((element) => {
-  //   let obj = {};
-  //   obj.date = element.datetime;
-  //   obj.description = element.weather.description;
-  //   return obj;
-  // });
-
-  response.send(arrayOfObjects);
-});
+      let weatherArray = weatherData.map((n) => {
+        return new Forecast(
+          n["datetime"],
+          n["weather"]["description"],
+          n["high_temp"],
+          n["low_temp"]
+        );
+      });
+      response.send(weatherArray);
+    })
+    .catch((err) => {
+      response.send(err);
+    });
+}
 
 server.get("*", (req, res) => {
   res.status(404).send("page not found");
