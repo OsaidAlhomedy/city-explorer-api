054de3f23d9c04b4658e83d718b6ad492627dedd
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-08-03 09:31:19 +0300

    added the constructor

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const weatherData = require("./data/weather.json");
 const server = express();
 const cors = require("cors");
 const PORT = process.env.PORT;
-let arrayOfObjects = [];
+const Forecast = require("./Forecast");
 
 server.use(cors());
 
@@ -17,29 +17,31 @@ server.get("/", (request, response) => {
 
 // http://localhost:3010/weather?q=<cityname>&lon=<the_longitude>&lat=<the_latitude>
 server.get("/weather", (request, response) => {
+  let arrayOfObjects = [];
   let name = request.query.q;
   name = name.charAt(0).toUpperCase() + name.slice(1);
   let lon = request.query.lon;
   let lat = request.query.lat;
 
   let weatherArr = weatherData.find((element) => name == element.city_name);
-  let dataArray = weatherArr.data.map((element) => {
-    let obj = {};
-    obj.date = element.datetime;
-    obj.description = element.weather.description;
-    return obj;
-  });
 
-  // class Forecast {
-  //   constructor(date, description) {
-  //     this.date = date;
-  //     this.description = description;
-  //     arrayOfObjects.push(this);
+  for (let i = 0; i < weatherArr.data.length; i++) {
+    arrayOfObjects.push(
+      new Forecast(
+        weatherArr.data[i].datetime,
+        weatherArr.data[i].weather.description
+      )
+    );
+  }
 
-  //   }
-  // }
+  // let dataArray = weatherArr.data.map((element) => {
+  //   let obj = {};
+  //   obj.date = element.datetime;
+  //   obj.description = element.weather.description;
+  //   return obj;
+  // });
 
-  response.send(dataArray);
+  response.send(arrayOfObjects);
 });
 
 server.get("*", (req, res) => {
